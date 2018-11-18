23034a389318931ed73f7a39bc376b86c5040098
江浪 <643546122@qq.com>
2018-11-18 19:15:14 +0800

    取消了postcss移动适配

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.less';
+import { List, Checkbox, Button } from 'antd-mobile';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const CheckboxItem = Checkbox.CheckboxItem;
 
 class App extends Component {
+  onChange = (val) => {
+    console.log(val);
+  }
   render() {
+    const data = [
+      { value: 0, label: 'Ph.D.' },
+      { value: 1, label: 'Bachelor' },
+      { value: 2, label: 'College diploma' },
+    ];
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="test"></div>
+        <Button type="primary">开始了</Button>
+        <List renderHeader={() => 'CheckboxItem demo'}></List>
+        {data.map(i => (
+          <CheckboxItem key={i.value} onChange={() => this.onChange(i.value)}>
+            {i.label}
+          </CheckboxItem>
+        ))}
       </div>
     );
   }
